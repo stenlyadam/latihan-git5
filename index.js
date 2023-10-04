1c291555af5ae13d97c768b8ab696750e7c94637
AndreasTopuh <andreasjeno23@gmail.com>
2023-10-04 09:44:09 +0800

    uji coba

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Basic Git and Github
 
 console.log("Perubahan di hari senin");
-<<<<<<< HEAD
 console.log("Poetri cantik hehe");
-=======
 console.log("Juan Keren");
 console.log("Hello world");
 console.log("front end menyenangkan");
->>>>>>> daccf1a046618e158fdb6e234cf38d66c7df71c8
 
 console.log("hallo ini andreas");

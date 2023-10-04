56ac59b1d3cc899b98a47b68927cec01f47c364f
unseven13 <j13karongkong@gmail.com>
2023-10-04 09:46:05 +0800

    tambah wil 2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 console.log("Perubahan di hari senin");
 
-console.log ("william anime banget")
+console.log("william anime banget")
 console.log("Poetri cantik hehe");
 console.log("Juan Keren");
 console.log("Hello world");
 console.log("front end menyenangkan");
 console.log("putri kedua");
 console.log("Hani Jastin Giovanni Ladjamba");
-console.log("perisai luar biasa dari kerajaan romawi")
+console.log("perisai luar biasa dari kerajaan romawi");

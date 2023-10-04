4131d94799cc670dfc2f93e79ec3dd08b9ea0677
EzraPelealu <pelealuezra3gmail.com>
2023-10-04 10:15:49 +0800

    cinta dari ezra

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ console.log("perisai luar biasa dari kerajaan romawi");
 console.log ("bubur bubur apa yang bisa besar? bubur zoom zoom")//by andreas topuh
 
 console.log("Ezra disini");
+console.log("Aku cinta doi");
 
 console.log("halo dek");
 

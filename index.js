3d71c1e84f9637110b5682f925167361b62e6994
AndreasTopuh <andreasjeno23@gmail.com>
2023-10-04 09:50:31 +0800

    Tambahan dari pemula

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ console.log("front end menyenangkan");
 console.log("hallo ini andreas");
 console.log("putri kedua");
 console.log("Hani Jastin Giovanni Ladjamba");
-<<<<<<< HEAD
 console.log("perisai luar biasa dari kerajaan romawi")
 
 console.log("HALO SEMUA INI DEAS");
-=======
 console.log("perisai luar biasa dari kerajaan romawi");
 
 console.log ("bubur bubur apa yang bisa besar? bubur zoom zoom")//by andreas topuh
->>>>>>> 5ca01f74acd1b8e87cd1f053363307c1a5c516fc
+
+
+console.log("halo dek");
